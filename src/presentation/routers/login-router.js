a7bddf0798ab07acd022cf89b3306fe5a331e6eb
Giovane Goncalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2021-11-16 16:21:06 -0300

    Added error log to login-router

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -26,7 +26,7 @@ module.exports = class LoginRouter{
       return HttpResponse.ok({accessToken})
     } 
     catch (error) {
-      // console.error(error)
+      console.error(error)
       return HttpResponse.serverError()      
     }
   }
