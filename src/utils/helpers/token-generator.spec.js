90c4401b9995dbc9e48f7fceb4c6a364ff0bd0d5
Giovane Goncalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2021-12-01 12:47:54 -0300

    ensure TokenGenerator returns a token if jwt returns a token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,7 +1,8 @@
+const jwt = require('jsonwebtoken')
 
 class TokenGenerator {
-  async generate(){
-    return null
+  async generate(id){
+    return jwt.sign(id, 'secret')
   }
 }
   
@@ -14,7 +15,14 @@ const makeSut = () => {
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
     const sut = makeSut()
+    jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  test('Should return a token if JWT returns token', async () => {
+    const sut = makeSut()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })
