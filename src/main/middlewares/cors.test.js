04b22423c72993bd95b78c6dbae16d2ba754c57b
thegiovanesilva <48033682+thegiovanesilva@users.noreply.github.com>
2021-12-11 20:08:32 -0300

    ensure requests parse body as JSON

--- src/main/middlewares/cors.test.js
+++ src/main/middlewares/cors.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest')
 const app = require('../config/app')
 
-describe('App Setup', () => {
-
+describe('CORS Middleware', () => {
   test('Should enable CORS', async () => {
     app.get('/test_cors', (req, res) => {
       res.send('')
     })
+
     const res = await request(app).get('/test_cors')
     expect(res.headers['access-control-allow-origin']).toBe('*')
     expect(res.headers['access-control-allow-methods']).toBe('*')
