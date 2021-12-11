39407a472669886f761065b4bf033cc3ac4c46f6
thegiovanesilva <48033682+thegiovanesilva@users.noreply.github.com>
2021-12-11 20:48:59 -0300

    ensure requests return content-type as xml if are forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -7,6 +7,20 @@ describe('Content-Type Middleware', () => {
       res.send('')
       
     })
-    await request(app).get('/test_content_type').expect('content-type', /json/)
+    await request(app)
+    .get('/test_content_type')
+    .expect('content-type', /json/)
   })
+
+  test('Should return xml content-type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+      
+    })
+    await request(app)
+    .get('/test_content_type_xml')
+    .expect('content-type', /xml/)
+  })
+   
 })
