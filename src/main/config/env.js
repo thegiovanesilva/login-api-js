072ed0401aac09c839696a777afcb47628a16a75
Giovane Goncalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2021-12-14 13:42:58 -0300

    fix mongodb and app port

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  tokenSecret: process.env.TOKEN_SECRET || 'secret'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/setup-project',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  port: process.env.port || 5858 
 }
