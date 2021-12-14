b450486607e44ec66fc2f10dfa722be6a1907256
Giovane Goncalves Da Silva <giovane.silva@expressosaomiguel.com.br>
2021-12-14 15:41:40 -0300

    fix

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -2,4 +2,4 @@ module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/setup-project',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
   port: process.env.port || 5858 
-}
+}  
